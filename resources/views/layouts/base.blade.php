7db7fccf452c6cdf08bff19ea71ae81deba443a0
Ibrahim <ibrahim.bougaoua@etu.usthb.dz>
2021-09-03 13:43:09 -0400

    add header

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -18,29 +18,21 @@
 </head>
 <body>
 <!-- Navigation-->
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-dark" style="position: fixed;width: 100%;z-index: 1;">
     <div class="container px-4 px-lg-5">
-        <a class="navbar-brand" href="#!">MyLogiciel</a>
+        <a class="navbar-brand" href="#!"><img src="{{ asset('assets/images/icons/logo.svg') }}" alt="" class="w-25 h-25"/></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
-                <li class="nav-item"><a class="nav-link active" aria-current="page" href="#!">Home</a></li>
-                <li class="nav-item"><a class="nav-link" href="#!">About</a></li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Shop</a>
-                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="#!">All Products</a></li>
-                        <li><hr class="dropdown-divider" /></li>
-                        <li><a class="dropdown-item" href="#!">Popular Items</a></li>
-                        <li><a class="dropdown-item" href="#!">New Arrivals</a></li>
-                    </ul>
-                </li>
+                <li class="nav-item"><a class="nav-link active font-weight-bold text-white" aria-current="page" href="#!">Home</a></li>
+                <li class="nav-item"><a class="nav-link font-weight-bold text-white" href="#!">About</a></li>
+                <li class="nav-item"><a class="nav-link font-weight-bold btn btn-sm btn-success text-white" href="#!">Booking</a></li>
             </ul>
             <ul class="navbar-nav ml-auto">
                 @if(Route::has('login'))
                     @auth
                             <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">{{ Auth::user()->name }}</a>
+                                <a class="nav-link dropdown-toggle text-white" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">{{ Auth::user()->name }}</a>
                                 <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
                                     <li><a class="dropdown-item" href="{{ route('admin.dashboard') }}">Dashboard</a></li>
                                     <li><hr class="dropdown-divider" /></li>
